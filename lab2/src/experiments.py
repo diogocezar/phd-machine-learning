22de0c8d5c50c72aed11eb1ad613552927ef9d28
Diogo Cezar Teixeira Batista <diogo.batista@meifacil.com>
2020-08-11 08:49:10 -0300

    Implementing othe classifiers

--- lab2/src/experiments.py
+++ lab2/src/experiments.py
@@ -1,12 +1,16 @@
 import sys
 import numpy
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.datasets import load_svmlight_file
 from sklearn import preprocessing
 from sklearn.metrics import f1_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Perceptron
 import pylab as pl
 import seaborn as sns
 import time
@@ -29,18 +33,42 @@ def get_test_base():
     return[x_array, y]
 
 
-def classify_knn(chunk):
+def classify_generic(classificator, chunk, start_time):
     x_train, y_train = get_chunk_train_base(chunk)
     x_test, y_test = get_test_base()
-    classificator = KNeighborsClassifier()
     classificator.fit(x_train, y_train)
     predict = classificator.predict(x_test)
-    result_f1_score = f1_score(
-        y_test, predict, labels=labels, average='weighted')
-    result_accuracy = classificator.score(x_test, y_test)
+    result_f1_score = round_float(f1_score(
+        y_test, predict, labels=labels, average='weighted'))
+    result_accuracy = round_float(classificator.score(x_test, y_test))
     result_conf_mat = confusion_matrix(y_test, predict)
-    result_time = get_time()
+    result_time = round_float(get_time(start_time))
     return[result_f1_score, result_accuracy, result_conf_mat, result_time]
+
+
+def classify_knn(chunk, start_time):
+    classificator = KNeighborsClassifier()
+    return classify_generic(classificator, chunk, start_time)
+
+
+def classify_naive_bayes(chunk, start_time):
+    classificator = GaussianNB()
+    return classify_generic(classificator, chunk, start_time)
+
+
+def classify_lda(chunk, start_time):
+    classificator = LinearDiscriminantAnalysis()
+    return classify_generic(classificator, chunk, start_time)
+
+
+def classify_logistic_regression(chunk, start_time):
+    classificator = LogisticRegression()
+    return classify_generic(classificator, chunk, start_time)
+
+
+def classify_perceptron(chunk, start_time):
+    classificator = Perceptron()
+    return classify_generic(classificator, chunk, start_time)
 
 
 def get_orchestrator():
@@ -92,9 +120,13 @@ def save_result(classifier,
         [classifier, start, stop, step, i, result_f1_score, result_accuracy, result_time])
 
 
-def get_time():
+def get_time(start_time):
     end_time = time.time()
     return end_time - start_time
+
+
+def round_float(value):
+    return float("{:.3f}".format(value))
 
 
 def run_orchestrator(orchestrator, start_time, table_writer):
@@ -104,29 +136,43 @@ def run_orchestrator(orchestrator, start_time, table_writer):
         start = int(experiment['chunk_start'])
         stop = int(experiment['chunk_stop'])
         step = int(experiment['chunk_step'])
+        classifier = str(experiment['classifier'])
         for i in range(start, stop, step):
+            start_time = time.time()
             print('Chunk: ' + str(i))
-            if str(experiment['classifier']) == "knn":
+            if classifier == "knn":
                 result_f1_score, result_accuracy, result_conf_mat, result_time = classify_knn(
-                    i)
-                print_result(result_f1_score,
-                             result_accuracy,
-                             result_conf_mat,
-                             result_time)
-                save_result("knn",
-                            start,
-                            stop,
-                            step,
-                            i,
-                            result_f1_score,
-                            result_accuracy,
-                            result_conf_mat,
-                            result_time,
-                            table_writer)
+                    i, start_time)
+            if classifier == "naive_bayes":
+                result_f1_score, result_accuracy, result_conf_mat, result_time = classify_naive_bayes(
+                    i, start_time)
+            if classifier == "lda":
+                result_f1_score, result_accuracy, result_conf_mat, result_time = classify_lda(
+                    i, start_time)
+            if classifier == "logistic_regression":
+                result_f1_score, result_accuracy, result_conf_mat, result_time = classify_logistic_regression(
+                    i, start_time)
+            if classifier == "perceptron":
+                result_f1_score, result_accuracy, result_conf_mat, result_time = classify_perceptron(
+                    i, start_time)
+
+            print_result(result_f1_score,
+                         result_accuracy,
+                         result_conf_mat,
+                         result_time)
+            save_result(classifier,
+                        start,
+                        stop,
+                        step,
+                        i,
+                        result_f1_score,
+                        result_accuracy,
+                        result_conf_mat,
+                        result_time,
+                        table_writer)
 
 
 if __name__ == "__main__":
-    start_time = time.time()
     orchestrator = get_orchestrator()
     tabulation_writer, tabulation_csv_file = get_tabulation()
     run_orchestrator(orchestrator, start_time, tabulation_writer)
